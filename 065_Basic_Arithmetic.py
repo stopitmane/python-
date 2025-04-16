8a372dab35512f9ff476d06bd64914f9d88f9c03
Ajayi John <ajayijohn19999@gmail.com>
2025-04-16 12:34:56 +0100

    Initial commit

---
+++ 065_Basic_Arithmetic.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Basic Arithmetic
+# 
+# This is just math. Basic math. Adding, subtracting, dividing, multiplying, exponents, and getting the remainder from a division (modulus)
+
+# ### Addition
+
+# In[2]:
+
+
+89 + 11
+
+
+# ### Subtraction
+
+# In[3]:
+
+
+100 - 49
+
+
+# ### Division
+
+# In[4]:
+
+
+100 / 10
+
+
+# ### Multiplication
+
+# In[5]:
+
+
+9 * 9
+
+
+# ### Exponents
+
+# In[6]:
+
+
+3**3
+
+
+# In[7]:
+
+
+3 * 3 * 3
+
+
+# ### Modulus
+# 
+# Modulus is the ability to get the remainder from a division. For example, 3 goes into 10 3 times and has a remainder of 1. So `10 % 3 = 1` 
+
+# In[8]:
+
+
+10 / 3 
+
+
+# In[9]:
+
+
+10 % 3
+
